6a88ba97d2f9c447864ae82532097d4becde946b
chenruipeng <1259903760@qq.com>
2024-07-12 17:03:14 +0800

    feat: 子项目初始化

---
+++ src/views/SubView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App22222222"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'SubView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
